dc16c4c584c64e437f09e8d677670590ddbf75b9
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-21 07:28:51 +0000

    create helper methods for current_user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,17 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  #helpers!
+  helpers do
+    def current_user
+      if session[:user_id] #if a session with a user_id exists (not nil)
+        @current_user = Chef.find_by(:id => session[:user_id])
+      end
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 
 end
