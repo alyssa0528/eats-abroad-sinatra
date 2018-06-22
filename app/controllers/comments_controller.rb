14db0433a04cf7e249afa72985ca7f7743af6221
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-22 17:03:55 +0000

    add GET edit route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,19 @@
 class CommentsController < ApplicationController
 
   get '/comments/:id/edit' do
+    @comment = Comment.find(params[:id])
+    @restaurant = @comment.restaurant
 
+    if logged_in?
+      if @comment.chef_id == current_user.id
+        erb :'/comments/edit'
+      else
+        redirect "/restaurants/#{@comment.restaurant_id}"
+      end
+    else
+      redirect '/login'
+    end
   end
+
+  
 end
