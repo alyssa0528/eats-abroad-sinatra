f16a2ba3d3c15851466e6d53566fa769c71d5e8a
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-24 16:34:07 +0000

    add rack flash 3 and error msgs to controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
-class CommentsController < ApplicationController
+require 'sinatra/base'
+require 'rack-flash'
 
+class CommentsController < ApplicationController
+use Rack::Flash
   #render edit form only if the selected comment is the current user's
   get '/comments/:id/edit' do
     @comment = Comment.find(params[:id])
@@ -9,6 +12,7 @@ class CommentsController < ApplicationController
       if @comment.chef_id == current_user.id
         erb :'/comments/edit'
       else
+        flash[:message] = "**Sorry, you do not have permission to edit this comment.**"
         redirect "/restaurants/#{@restaurant.id}" #raise error saying user doesn't have permission to edit
       end
     else
@@ -24,13 +28,16 @@ class CommentsController < ApplicationController
     if logged_in?
       if @comment.chef_id == current_user.id
         if params[:comments][:content].empty? #if the submitted comment is blank
+          flash[:message] = "**The comment field cannot be blank.**"
           erb :'/comments/edit' #raise error saying that a comment cannot be blank!
         else
           @comment.content = params[:comments][:content] #update @comment's content value with the new comment
           @comment.save #save to db
+          flash[:message] = "**Comment successfully updated.**"
           redirect "/restaurants/#{@restaurant.id}" #redirect to restaurant's page
         end
       else
+        flash[:message] = "**Sorry, you do not have permission to edit this comment.**"
         redirect "/restaurants/#{@restaurant.id}" #raise error saying user doesn't have permission to edit
       end
     else
@@ -47,8 +54,10 @@ class CommentsController < ApplicationController
     if logged_in?
       if current_user.id == @comment.chef_id
         @comment.delete
+        flash[:message] = "**Comment successfully deleted.**"
         redirect "/restaurants/#{@restaurant.id}" #maybe raise message saying your comment has been successfully deleted?
       else
+        flash[:message] = "**Sorry, you do not have permission to delete this comment.**"
         redirect "/restaurants/#{@restaurant.id}" #error message saying you don't have permission to delete this comment
       end
     else
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,8 @@
+require 'sinatra/base'
+require 'rack-flash'
+
 class RestaurantsController < ApplicationController
+  use Rack::Flash
 
   #shows list of all recommended restaurants
   get '/restaurants' do
@@ -22,18 +26,21 @@ class RestaurantsController < ApplicationController
   post '/restaurants' do
     if logged_in?
       if params[:restaurant].empty? && params[:comments].empty? #if user submits totally blank form
+        flash[:message] = "**Please select a restaurant from the list below or add a new one.**"
         redirect '/restaurants/new' # error message asking chef to please select or add a restaurant
       elsif params[:comments][:content].empty?
+        flash[:message] = "**You must enter a comment.**"
         redirect '/restaurants/new' #error message here that comment field cannot be empty
       elsif params[:restaurant][:name].empty? #if a user selects an existing restaurant from the list
         @restaurant = Restaurant.find_by(:id => params[:restaurant][:id])
-        #@restaurant.chefs << Chef.new(:name => current_user.name)
         @restaurant.comments << Comment.new(:content => params[:comments][:content], :chef_id => current_user.id)
+        flash[:message] = "**Restaurant successfully added to your list.**"
         redirect "/restaurants/#{@restaurant.id}"
       else
         @restaurant = Restaurant.new(params[:restaurant]) #if a brand new restaurant is added by user w/all inputs filled out
         @restaurant.save #saves to database and creates a @restaurant ID
         @restaurant.comments << Comment.new(:content => params[:comments][:content], :chef_id => current_user.id)
+        flash[:message] = "**Restaurant successfully added to your list.**"
         redirect "/restaurants/#{@restaurant.id}"
       end
     else
